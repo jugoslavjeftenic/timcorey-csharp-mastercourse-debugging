621127a24b12a164854ffbb7abbaf8b664749c6b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-18 18:58:08 +0200

    UsingBreakpoints

---
+++ T12_01_UsingBreakpoints/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace T12_01_UsingBreakpoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"The value of i is {i}.");
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine($"The value of j is {j}.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
