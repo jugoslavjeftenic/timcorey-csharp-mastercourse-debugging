b6668b9c6519cdd4df49f786cbcc408ec85fcb4c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-18 20:23:05 +0200

    HandlingExceptions

---
+++ T12_02_01_HandlingExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace T12_02_01_HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BadCall();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("We have a problem.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void BadCall()
+        {
+            int[] ages = { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Houston, we have a problem.");
+                    Console.WriteLine(ex);
+                    throw new Exception("Error handling array.", ex);
+                }
+            }
+        }
+    }
+}
