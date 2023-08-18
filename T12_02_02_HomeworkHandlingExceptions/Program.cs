05b5091b085b28dcce8d256822317633edb39e55
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-18 20:37:03 +0200

    HomeworkHandlingExceptions

---
+++ T12_02_02_HomeworkHandlingExceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace T12_02_02_HomeworkHandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Console Application with a for loop that
+            // throws an exception after five iterations.
+            // Catch the exception.
+
+            try
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                    if (i == 5)
+                    {
+                        throw new Exception("Thrown exception.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
