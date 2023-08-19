9af27a1170bda76930163dc30575419519597f4d
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-19 15:22:28 +0200

    HomeworkAdvancedBreakpoints

---
+++ T13_02_02_HomeworkAdvancedBreakpoints/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace T13_02_02_HomeworkAdvancedBreakpoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Console Application that loops from 1 to 100.
+            // Throw an exception on 73.
+            // Use a breakpoint to break before the breaking situation.
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 73)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+    }
+}
