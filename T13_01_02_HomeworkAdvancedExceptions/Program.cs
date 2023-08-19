3af2149c322baffbc061a20a7bf67f19eedbd1d7
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-19 14:21:41 +0200

    HomeworkAdvancedExceptions

---
+++ T13_01_02_HomeworkAdvancedExceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace T13_01_02_HomeworkAdvancedExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Console Application that throws an exception
+            //in a method that you catch in the main method.
+
+            try
+            {
+                CreateException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private static void CreateException()
+        {
+            Console.WriteLine("2 + 2 = 5");
+            throw new ArithmeticException("Something is wrong with this method.");
+        }
+    }
+}
